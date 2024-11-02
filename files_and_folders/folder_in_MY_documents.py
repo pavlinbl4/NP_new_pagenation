d4a62224ed52aa2d4b2ac356258d6ba9555da6b8
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-11-02 10:55:25 +0300

    change path to download folder

--- files_and_folders/folder_in_MY_documents.py
+++ files_and_folders/folder_in_MY_documents.py
@@ -2,10 +2,11 @@ from pathlib import Path
 
 
 def make_documents_folder(name):
-    (Path.home() / "Documents" / f"{name}").mkdir(parents=True, exist_ok=True)
+    # (Path.home() / "Documents" / f"{name}").mkdir(parents=True, exist_ok=True)
+    x = (Path.home() / f"{name}/{name}").mkdir(parents=True, exist_ok=True)
 
-    return Path.home() / "Documents" / f"{name}"
-
+    # return Path.home() / "Documents" / f"{name}"
+    return Path.home() / f"{name}*2/{name}"
 
 if __name__ == '__main__':
-    make_documents_folder(input("Enter the folder name\n\n"))
+    print(make_documents_folder(input("Enter the folder name\n\n")))
