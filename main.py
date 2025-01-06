74f91274c813c43ab47ffcd77f26bc90dd4fda85
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-01-06 17:06:55 +0300

    Функция `cook_soup` не обрабатывает возможные ошибки HTTP-запросов. Если сервер вернёт ошибку (например, код 404 или 500), программа упадёт. Решение: Добавьте проверку статуса ответа

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from requests import Session
 
 from gui_tools.select_month import select_month
 from image_downloader import downloader
+from loguru import logger
 
-
+logger.disable('__main__')
 # pip install lxml
 
 icloud_folder = Path().home() / 'Library/Mobile Documents/com~apple~CloudDocs/'
@@ -28,7 +29,14 @@ def cook_soup(count, base_url):
     s = Session()
     s.headers.update(headers)
     work_url = f'{base_url}/?article={count}&pageId=1&hash=cbf05cb35393866fb263316a822d653e4971ece5'
-    response = s.get(work_url)
+
+    try:
+        response = s.get(work_url)
+        response.raise_for_status()  # Проверяет статус ответа
+    except Exception as e:
+        logger.error(f"Ошибка при запросе {work_url}: {e}")
+        return None
+
     soup = BeautifulSoup(response.text, 'lxml')
     return soup
 
@@ -54,6 +62,10 @@ def extract_article_data(base_url, soup, month_number):
 
         # get date of article
         article_date = article.find('div', class_="tplarticle-date").text
+        logger.info(f"{article_date = }")
+        article_year = article_date.split(' ')[2]
+        logger.info(f"{article_year = }")
+
 
         # if found name of last month stop working
         if previous_month_name in article_date:
@@ -67,12 +79,20 @@ def extract_article_data(base_url, soup, month_number):
         if month_name in article_date:
             print(article_date, article_name, article_image_link)
 
-            (icloud_folder / f"Documents/NewProspect/2024_{month_number}").mkdir(parents=True, exist_ok=True)
+            (icloud_folder / f"Documents/NewProspect/{article_year}_{month_number}").mkdir(parents=True, exist_ok=True)
             # download images
             downloader(article_image_link, image_name,
-                       folder_path=(icloud_folder / f"Documents/NewProspect/2024_{month_number}"))
+                       folder_path=(icloud_folder / f"Documents/NewProspect/{article_year}_{month_number}"))
 
 
 if __name__ == '__main__':
     _month_number = select_month()
     main(_month_number)
+
+
+    soup = cook_soup(1, 'https://newprospect.ru')
+    print(extract_article_data('https://newprospect.ru',
+                               soup, 1))
+
+
+
